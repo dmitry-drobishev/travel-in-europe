5b6129531782a997f3591ebdbfcae9ae2868c22d
dmitry-drobishev <mitya.drobishev.77rus@yandex.ru>
2022-02-17 02:08:25 +0300

    мобилка по пиксель перфект, планшет и десктоп-черновой вариант

--- source/js/script.js
+++ source/js/script.js
@@ -1,72 +1,93 @@
+//main navigation
 const navMain = document.querySelector('.main-nav');
-const navToggle = document.querySelector('.main-nav__toggle');
+const navToggle = document.querySelector('.page-header__toggle');
 
-navMain.classList.remove('main-nav--nojs');
+  navMain.classList.remove('main-nav--nojs');
+  navToggle.classList.remove('page-header__toggle--nojs');
 
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-  }
-});
-
-const link = document.querySelector('.js-login');
-const popup = document.querySelector('.modal');
-const close = popup.querySelector('.modal__button--close');
-const form = popup.querySelector('form');
-const login = popup.querySelector('[name=login]');
-const password = popup.querySelector('[name=password]');
-
-let isStorageSupport = true;
-let storage = '';
-
-try {
-  storage = localStorage.getItem('login');
-} catch (err) {
-  isStorageSupport = false;
-}
-
-link.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  popup.classList.add('modal--show');
-
-  if (storage) {
-    login.value = storage;
-    password.focus();
-  } else {
-    login.focus();
-  }
-});
-
-close.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  popup.classList.remove('modal--show');
-  popup.classList.remove('modal--error');
-});
-
-form.addEventListener('submit', function (evt) {
-  if (!login.value || !password.value) {
-    evt.preventDefault();
-    popup.classList.remove('modal--error');
-    popup.offsetWidth = popup.offsetWidth;
-    popup.classList.add('modal--error');
-  } else {
-    if (isStorageSupport) {
-      localStorage.setItem('login', login.value);
+  navToggle.addEventListener('click', function() {
+    if (navMain.classList.contains('main-nav--close')) {
+      navMain.classList.remove('main-nav--close');
+      navMain.classList.add('main-nav--open');
+      navToggle.classList.remove('page-header__toggle--close');
+      navToggle.classList.add('page-header__toggle--open');
+    } else {
+      navMain.classList.add('main-nav--close');
+      navMain.classList.remove('main-nav--open');
+      navToggle.classList.add('page-header__toggle--close');
+      navToggle.classList.remove('page-header__toggle--open');
     }
-  }
-});
+  });
 
-window.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === 27) {
-    evt.preventDefault();
+// const navMain = document.querySelector('.main-nav');
+// const navToggle = document.querySelector('.main-nav__toggle');
 
-    if (popup.classList.contains('modal--show')) {
-      popup.classList.remove('modal--show');
-      popup.classList.remove('modal--error');
-    }
-  }
-});
+// navMain.classList.remove('main-nav--nojs');
+
+// navToggle.addEventListener('click', function() {
+//   if (navMain.classList.contains('main-nav--closed')) {
+//     navMain.classList.remove('main-nav--closed');
+//     navMain.classList.add('main-nav--opened');
+//   } else {
+//     navMain.classList.add('main-nav--closed');
+//     navMain.classList.remove('main-nav--opened');
+//   }
+// });
+
+// const link = document.querySelector('.js-login');
+// const popup = document.querySelector('.modal');
+// const close = popup.querySelector('.modal__button--close');
+// const form = popup.querySelector('form');
+// const login = popup.querySelector('[name=login]');
+// const password = popup.querySelector('[name=password]');
+
+// let isStorageSupport = true;
+// let storage = '';
+
+// try {
+//   storage = localStorage.getItem('login');
+// } catch (err) {
+//   isStorageSupport = false;
+// }
+
+// link.addEventListener('click', function (evt) {
+//   evt.preventDefault();
+//   popup.classList.add('modal--show');
+
+//   if (storage) {
+//     login.value = storage;
+//     password.focus();
+//   } else {
+//     login.focus();
+//   }
+// });
+
+// close.addEventListener('click', function (evt) {
+//   evt.preventDefault();
+//   popup.classList.remove('modal--show');
+//   popup.classList.remove('modal--error');
+// });
+
+// form.addEventListener('submit', function (evt) {
+//   if (!login.value || !password.value) {
+//     evt.preventDefault();
+//     popup.classList.remove('modal--error');
+//     popup.offsetWidth = popup.offsetWidth;
+//     popup.classList.add('modal--error');
+//   } else {
+//     if (isStorageSupport) {
+//       localStorage.setItem('login', login.value);
+//     }
+//   }
+// });
+
+// window.addEventListener('keydown', function (evt) {
+//   if (evt.keyCode === 27) {
+//     evt.preventDefault();
+
+//     if (popup.classList.contains('modal--show')) {
+//       popup.classList.remove('modal--show');
+//       popup.classList.remove('modal--error');
+//     }
+//   }
+// });
